d55318cbbf92add7d301c1e243e536a361711240
Laurence Apuya pangilinan <lapuya-p@c1r10s6.42madrid.com>
2021-04-07 14:03:32 +0200

    added strlcpy

---
+++ Libft/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lapuya-p <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 13:42:21 by lapuya-p          #+#    #+#             */
+/*   Updated: 2021/04/07 13:44:57 by lapuya-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	n;
+
+	i = 0;
+	n = 0;
+	while (src[n] != '\0')
+		n++;
+	if (size > 0)
+	{
+		i = 0;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (n);
+}
