ac8fed8a9599788ba27a31f62eabc051619c9999
Laurence Apuya pangilinan <lapuya-p@c1r10s3.42madrid.com>
2021-05-14 09:02:13 +0200

    commented the code

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lapuya-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 09:02:40 by lapuya-p          #+#    #+#             */
-/*   Updated: 2021/05/04 10:03:35 by lapuya-p         ###   ########.fr       */
+/*   Updated: 2021/05/13 09:50:01 by lapuya-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,14 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
+/*	save_and_point returns the pointer to line
+	1. We search for the \n or null
+	2. if its a \n
+		-we store in line until \n
+		-we save the rest of the string in the static variable
+	3. if it's \0, we store the string saved in the static variable
+*/
+
 char	*ft_save_and_point(char *save, char **line)
 {
 	unsigned int	i;
@@ -80,6 +88,12 @@ char	*ft_save_and_point(char *save, char **line)
 	return (save);
 }
 
+/*	join_and_adjust adds the line we got (aux) with the new line read (buf)
+	-we assign to a tmp variable the product of joining the two strings
+	-we delete what we got and assign him the new string (tmp)
+	-we delete the tmp and return the new generated string
+*/
+
 char	*ft_join_and_adjust(char *aux, char *buf)
 {
 	char	*tmp;
@@ -90,6 +104,18 @@ char	*ft_join_and_adjust(char *aux, char *buf)
 	free(tmp);
 	return (aux);
 }
+
+/*	get_next_line gets a line from a file according to the buffer_size
+	-static to preserve its value out of its scope.
+	-4096 is the limit of files open.
+	-BUFFER_SIZE + 1 to add NULL-terminated to the string read
+	Reads and:
+		-if ret is -1, there was an error -> returns -1
+		-we join what we had with the new read
+		-we search for a \n from the line we read and:
+			1. if found, we store until the \n
+			2. if not, we read again and repeat
+ */
 
 int	get_next_line(int fd, char **line)
 {
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: lapuya-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 09:06:28 by lapuya-p          #+#    #+#             */
-/*   Updated: 2021/05/04 10:42:57 by lapuya-p         ###   ########.fr       */
+/*   Updated: 2021/05/13 12:48:39 by lapuya-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,9 @@ int	main()
 	char	*line;
 	
 	fd = open("example4.txt", O_RDONLY);
-	
 	while ((next_line = get_next_line(fd, &line)) != 0)
 	{
-		printf("%d\n", next_line);
+		//printf("%d\n", next_line);
 		printf("%s\n", line);
 		free(line);
 	}
